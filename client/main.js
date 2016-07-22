0c3b456b0b71e5ad54af99795a9918324cf19543
Jann3 <jannecleary@hotmail.com>
2016-07-22 01:09:02 +0100

    adding basic session interaction to navbar

--- client/main.js
+++ client/main.js
@@ -1,4 +1,21 @@
-// no interface
+Template.navbar.events({
+  'click .js-headset': function (event) {
+    event.preventDefault();
+
+    if($(event.currentTarget).closest("li").hasClass("active")){
+      delete Session.keys['headset'];
+      $(".js-headset").parents().removeClass("active");
+    } else {
+      Session.set('headset', $(event.currentTarget).closest("a").html());
+      $(".js-headset").parents().removeClass("active");
+      $(event.currentTarget).closest("li").addClass("active"); 
+    }
+
+    // echo headset from session
+    console.log(Session.get('headset'));
+  }
+});
+
 
 Meteor.call('getRift', function(error, result) {
   console.log(result);
