4255a8c3c3bf6445c67282cc45fcc4a6a1074438
Jann3 <jannecleary@hotmail.com>
2016-07-24 00:24:04 +0100

    created helper function for checking steam and oculus support before generating links

--- client/main.js
+++ client/main.js
@@ -36,6 +36,51 @@ Template.vr_list.helpers({
   }
 });
 
+Template.vr_title.helpers({
+  getSteamLink:function(support_rift, support_vive, steam_id){
+    if(!steam_id){
+      // no steam_id, no link
+      return false;
+    }
+
+    if(!Session.get('headset')){
+      // no headset selected, return link
+      return '<a href="http://store.steampowered.com/app/' + steam_id + '/" target="_blank">Steam</a>';
+    }
+
+    // check if headset is supported before returning steam link
+    // reduce duplicate code later
+    if(Session.get('headset')=='Rift'&&support_rift==true){
+      return '<a href="http://store.steampowered.com/app/' + steam_id + '/" target="_blank">Steam</a>';
+    } else if (Session.get('headset')=='Vive'&&support_vive==true){
+      return '<a href="http://store.steampowered.com/app/' + steam_id + '/" target="_blank">Steam</a>';
+    } else {
+      return false;
+    }
+  },
+  getOculusLink:function(support_rift, support_vive, rift_id){
+    if(!rift_id){
+      // no oculus_id, no link
+      return false;
+    }
+
+    if(!Session.get('headset')){
+      // no headset selected, return link
+      return '<a href="https://www.oculus.com/experiences/rift/' + rift_id + '/" target="_blank">Oculus</a>';
+    }
+
+    // check if headset is supported before returning steam link
+    // reduce duplicate code later
+    if(Session.get('headset')=='Rift'&&support_rift==true){
+      return '<a href="https://www.oculus.com/experiences/rift/' + rift_id + '/" target="_blank">Oculus</a>';
+    } else if (Session.get('headset')=='Vive'&&support_vive==true){
+      return '<a href="https://www.oculus.com/experiences/rift/' + rift_id + '/" target="_blank">Oculus</a>';
+    } else {
+      return false;
+    }
+  }
+});
+
 
 Meteor.call('getRift', function(error, result) {
   console.log(result);
