195da33a24ae450c7a4dbe512ceec54ca8f798e0
Jann3 <jannecleary@hotmail.com>
2016-07-31 22:59:16 +0100

    removed web scraping test

--- server/main.js
+++ server/main.js
@@ -93,29 +93,7 @@ DDPRateLimiter.addRule({
 
 
 
-// HTTP GET from oculus for Rift
 
-HTTP.call('GET',
-          'https://www.oculus.com/experiences/rift/',
-          { headers: { 'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36'},
-          followRedirects: false }, function(error, response){
-
-  if (error){
-    console.log(error);
-  } else {
-
-    // load response into cheerio
-
-    var $ = cheerio.load(response.content);
-
-    // search for Browse all
-
-    var browseAll = $('._4my5:contains("Browse all")').html();
-
-    console.log(browseAll);
-
-    }
-  }); // End HTTP GET
 
 // Publish entire VR
 
